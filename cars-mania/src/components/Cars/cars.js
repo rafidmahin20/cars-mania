b1fecf79df8659e3a31a1b2bd590339830787fe4
rafidmahin20 <submanmahin@gmail.com>
2022-03-26 11:51:59 +0600

    edited

--- cars-mania/src/components/Cars/cars.js
+++ cars-mania/src/components/Cars/cars.js
@@ -3,7 +3,7 @@ import React from 'react';
 const cars = (props) => {
     const {name, image, price} = props.data;
     return (
-        <div>
+        <div className='cars'>
             <img src={image} alt=""></img>
         </div>
     );
